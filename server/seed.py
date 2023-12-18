f39f23e43c60a3dd453616e25b923bdfd3d448c8
john makateto <jmakateto@gmail.com>
2023-12-18 14:16:02 +0300

    updates test0

--- server/seed.py
+++ server/seed.py
@@ -10,21 +10,23 @@ from models import db, Zookeeper, Animal, Enclosure
 fake = Faker()
 
 with app.app_context():
-
     Animal.query.delete()
     Zookeeper.query.delete()
     Enclosure.query.delete()
+    db.session.commit()
 
     zookeepers = []
     for n in range(25):
-        zk = Zookeeper(name=fake.name(), birthday=fake.date_between(
-            start_date='-70y', end_date='-18y'))
+        zk = Zookeeper(
+            name=fake.name(),
+            birthday=fake.date_between(start_date="-70y", end_date="-18y"),
+        )
         zookeepers.append(zk)
 
     db.session.add_all(zookeepers)
 
     enclosures = []
-    environments = ['Desert', 'Pond', 'Ocean', 'Field', 'Trees', 'Cave', 'Cage']
+    environments = ["Desert", "Pond", "Ocean", "Field", "Trees", "Cave", "Cage"]
 
     for n in range(25):
         e = Enclosure(environment=rc(environments), open_to_visitors=rc([True, False]))
@@ -33,13 +35,22 @@ with app.app_context():
     db.session.add_all(enclosures)
 
     animals = []
-    species = ['Lion', 'Tiger', 'Bear', 'Hippo', 'Rhino', 'Elephant', 'Ostrich',
-        'Snake', 'Monkey']
+    species = [
+        "Lion",
+        "Tiger",
+        "Bear",
+        "Hippo",
+        "Rhino",
+        "Elephant",
+        "Ostrich",
+        "Snake",
+        "Monkey",
+    ]
 
     for n in range(200):
         name = fake.first_name()
         while name in [a.name for a in animals]:
-            name=fake.first_name()
+            name = fake.first_name()
         a = Animal(name=name, species=rc(species))
         a.zookeeper = rc(zookeepers)
         a.enclosure = rc(enclosures)
@@ -47,4 +58,3 @@ with app.app_context():
 
     db.session.add_all(animals)
     db.session.commit()
-
